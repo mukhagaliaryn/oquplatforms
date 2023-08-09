670026aed581ab8f5f9ae204464af32a15412494
mukhagaliaryn <mukhagaliaryn@gmail.com>
2023-08-09 22:44:20 +0600

    Done

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from "../layouts/main";
+
+const About = () => {
+    return (
+        <MainLayout>
+            <h1>About page</h1>
+        </MainLayout>
+    )
+}
+
+export default About;
